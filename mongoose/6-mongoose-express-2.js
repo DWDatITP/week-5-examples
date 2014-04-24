4b1c3cc0ccea9de024a37496cb07027d8214aecd
Mike Dory <mike@dory.me>
2014-04-24 11:42:22 -0400

    Initial commit
    rewriting history
    
    adds
    
    rewriting history
    
    rewriting history

---
+++ mongoose/6-mongoose-express-2.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.set('port', process.env.PORT || 3000);
+
+// connect to mongo
+var mongoose = require('mongoose');
+// mongoose.connect('mongodb://people:people@ds049237.mongolab.com:49237/dwd-2014');
+mongoose.connect('mongodb://localhost/test');
+
+// define a schema
+var Person = mongoose.model('Person', {
+    name: String,
+    dateAdded: Date,
+    active: Boolean
+});
+
+
+// get all people
+app.get('/', function (req, res) {
+  Person.find({}, function (err, data) {
+      if (err) return console.error(err);
+      res.json(data);
+  });
+});
+
+
+// get a person by name
+app.get('/people/:person', function (req, res) {
+  Person.findOne({name: req.params.person})
+    .sort('-dateAdded')
+    .exec(function (err, data) {
+      if (!err) {
+        console.error(err);
+        res.status(500).json({status: "Error!"});
+      } else {
+        if (data) {
+          res.json(data);
+        } else {
+          res.send(404);
+        }
+      }
+  });
+});
+
+// get a person by name
+app.post('/person', function (req, res) {
+  console.log(req.body);
+
+  var personData = {
+    name : req.body.name,
+    active: true
+  };
+
+  p = new Person(personData);
+  p.save();
+
+  res.json({status:"Success!"});
+});
+
+// fire up the server
+app.listen(app.get('port'));
+console.log('firing up on port %d', app.get('port'));
